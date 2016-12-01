1633c6492cbff3dd49a5816d02c197971f3c2276
laivietanh1986 <laivietanh1986@gmail.com>
2016-12-01 18:57:11 +0700

    update with claim authentication project

---
+++ DemoClaimAuthentication/Controllers/HomeController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Services;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Security.Claims;
+
+namespace DemoClaimAuthentication.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [Authorize]
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            var model = new LoginModel();
+            return View(model);
+        }
+        [HttpPost]
+        public ActionResult Login(LoginModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                if (model.UserName == model.Password)
+                {
+                    var claims = new List<Claim>()
+                    {
+                        new Claim(ClaimTypes.Name,model.UserName),
+                        new Claim(ClaimTypes.Country,"Viet Nam")
+                    };
+                    var principal = new ClaimsPrincipal(new ClaimsIdentity(claims));
+                    var tranformer =
+                        FederatedAuthentication.FederationConfiguration.IdentityConfiguration.ClaimsAuthenticationManager;
+                    tranformer.Authenticate(string.Empty,principal);
+                    return RedirectToAction("Index");
+
+
+
+                }
+            }
+            return View();
+        }
+
+        
+        public ActionResult Logout()
+        {
+            FederatedAuthentication.SessionAuthenticationModule.SignOut();
+            return RedirectToAction("Index");
+        }
+    }
+
+    public class LoginModel
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ DemoClaimAuthentication/extension/ClaimTranformation.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IdentityModel.Services;
+using System.IdentityModel.Tokens;
+using System.Linq;
+using System.Security.Claims;
+using System.Web;
+
+
+
+namespace DemoClaimAuthentication.extension
+{
+    public class ClaimTranformation: ClaimsAuthenticationManager
+    {
+        public override ClaimsPrincipal Authenticate(string resourceName, ClaimsPrincipal incomingPrincipal)
+        {
+            if (incomingPrincipal.Identity.IsAuthenticated)
+            {
+                return base.Authenticate(resourceName, incomingPrincipal);
+            }
+            var newprincipal =  CreateClaimsPrincipal(incomingPrincipal);
+            CreateSessionSecurityTocken(newprincipal);
+            return newprincipal;
+        }
+
+        private void CreateSessionSecurityTocken(ClaimsPrincipal newprincipal)
+        {
+            var sessionToken = new SessionSecurityToken(newprincipal,TimeSpan.FromHours(8));
+            FederatedAuthentication.SessionAuthenticationModule.WriteSessionTokenToCookie(sessionToken);
+        }
+
+        private ClaimsPrincipal CreateClaimsPrincipal(ClaimsPrincipal incomingPrincipal)
+        {
+            var claims = new List<Claim>();
+            claims.Add(new Claim(ClaimTypes.Name,incomingPrincipal.Identity.Name) );
+            claims.Add(new Claim(ClaimTypes.Country,"Viet Nam"));
+            return new ClaimsPrincipal(new ClaimsIdentity(claims,"Custom"));
+        }
+
+    }
+}
